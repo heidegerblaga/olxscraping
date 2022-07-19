d0cad32d922c1350431daa6214c0f4299242ff30
Paweł <skyrimpluta@gmail.com>
2022-07-19 16:46:41 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium import webdriver
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.olx.pl/d/nieruchomosci/mieszkania/sprzedaz/")
+
+
+
