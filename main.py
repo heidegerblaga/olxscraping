3060e0656a3707408354581387b9412b7219fc63
Paweł <skyrimpluta@gmail.com>
2022-08-02 19:02:51 +0200

    24:48

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from selenium.webdriver.common.keys import Keys
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 import time
-from selenium import webdriver
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.olx.pl/d/nieruchomosci/mieszkania/sprzedaz/")
+s = HTMLSession()
+
+url = "https://www.olx.pl/nieruchomosci/"
+
+
+
+
+
+r = s.get(url)
+soup = BeautifulSoup(r.content,'html.parser')
+
+for offer in soup.find_all('div',class_='offer-wrapper'):
+    footer = offer.find ('td', class_='bottom-cell')
+    location = footer.find('small', class_='breadcrumb').get_text().strip().split(',')[0]
+    print(location)
+
+#print(getdata(url))
+
 
 
 
